3a85e6856274addbec0804c85247f2d2b4a52347
M1mi1010 <112493192+M1mi1010@users.noreply.github.com>
2026-07-13 18:35:23 +0100

    Update 867-Transpose_Matrix.java

--- Arrays/0867/867-Transpose_Matrix.java
+++ Arrays/0867/867-Transpose_Matrix.java
@@ -1,7 +1,5 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
-        // If a square matrix
-        // if (matrix.length == matrix[0].length) {
         int[][] matrix2 = new int[matrix[0].length][matrix.length]; 
 
         for (int i = 0; i < matrix.length; i++) {
