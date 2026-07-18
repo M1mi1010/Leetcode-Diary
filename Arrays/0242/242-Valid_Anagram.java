99ea4c280b15b1263b2c52d220f42f207e38440c
M1mi1010 <112493192+M1mi1010@users.noreply.github.com>
2026-07-18 20:41:52 +0100

    Create 242-Valid_Anagram.java

---
+++ Arrays/0242/242-Valid_Anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        if (s.isEmpty()) return true;
+
+        // Count of each character should be same
+        int[] count = new int[26];
+        
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (count[t.charAt(i) - 'a']-- <= 0) return false;
+        }
+
+        return true;
+    }
+}
