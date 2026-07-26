2176397b43c07901283d102946fb90a73b009952
M1mi1010 <112493192+M1mi1010@users.noreply.github.com>
2026-07-26 10:42:59 +0100

    Create 160-Intersection_of_Two_Linked_Lists.java

---
+++ Linked List Manipulation Patterns/0160/160-Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //while the end of the list hasnt been reached
+        int aEndReached = 0;
+        int bEndReached = 0;
+        ListNode currentA = headA;
+        ListNode currentB = headB;
+
+        while (aEndReached != 2 && bEndReached != 2) {
+            if (currentA == currentB) return currentA;
+            
+            if (currentA == null) {
+                aEndReached++;
+                currentA = headB;
+                continue;
+            }
+            if (currentB == null) {
+                bEndReached++;
+                currentB = headA;
+                continue;
+            }
+
+            currentA = currentA.next;
+            currentB = currentB.next;
+        }
+        return null;
+    }
+}
