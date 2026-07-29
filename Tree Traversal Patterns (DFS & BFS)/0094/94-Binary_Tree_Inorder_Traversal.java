2c645f91c72ed462ba3b50e7aa795086c29cd35d
M1mi1010 <112493192+M1mi1010@users.noreply.github.com>
2026-07-29 18:23:48 +0100

    Create 94-Binary_Tree_Inorder_Traversal.java

---
+++ Tree Traversal Patterns (DFS & BFS)/0094/94-Binary_Tree_Inorder_Traversal.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+
+    //Left node right
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) return list;
+
+        inorder(root);
+        return list;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+
+        if (root.left != null) {
+            inorder(root.left);
+        }
+
+        list.add(root.val);
+        
+        if (root.right != null) {
+            inorder(root.right);
+        }
+
+    }
+}
