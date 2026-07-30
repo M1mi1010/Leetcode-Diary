ad60765f3e3ebad8e2938265c540ff0b93fb08c0
M1mi1010 <112493192+M1mi1010@users.noreply.github.com>
2026-07-30 17:16:24 +0100

    Create 145-Binary_Tree_Postorder_Traversal.java

---
+++ Tree Traversal Patterns (DFS & BFS)/0145/145-Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postorder(root);
+        return list;
+    }
+
+    private void postorder(TreeNode root) {
+        if (root == null) return;
+
+        if (root.left != null) {
+            postorder(root.left);
+        }
+
+        if (root.right != null) {
+            postorder(root.right);
+        }
+
+        list.add(root.val);
+    }
+}
