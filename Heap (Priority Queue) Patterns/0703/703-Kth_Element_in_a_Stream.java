09b7dc2cbeb23574a63ea6989e2f87e440e0b9e7
M1mi1010 <112493192+M1mi1010@users.noreply.github.com>
2026-08-10 13:06:56 +0100

    Create 703-Kth_Element_in_a_Stream.java

---
+++ Heap (Priority Queue) Patterns/0703/703-Kth_Element_in_a_Stream.java
@@ -0,0 +1,31 @@
+class KthLargest {
+    PriorityQueue<Integer> pq;
+    final int size;
+    
+    public KthLargest(int k, int[] nums) {
+        pq = new PriorityQueue<>();
+        size = k;
+
+        for (int num : nums) {
+            pq.offer(num);
+            if (pq.size() > size) {
+                pq.poll();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if (pq.size() > size) {
+            pq.poll();
+        }
+
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
