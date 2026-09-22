8be22ca10f996bd1e2b63b4e8405638d1fccb390
M1mi1010 <112493192+M1mi1010@users.noreply.github.com>
2026-09-22 16:17:32 +0100

    Create 121-Best_Time_to_Buy_and_Sell_Stock.java

---
+++ DP/0121/121-Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minVal = Integer.MAX_VALUE;
+        for (int i = 0 ; i < prices.length; i++) {
+            if (prices[i] < minVal) minVal = prices[i];
+            if (prices[i] - minVal > maxProfit) maxProfit = prices[i] - minVal;
+        }
+
+        return maxProfit;
+    }
+}
